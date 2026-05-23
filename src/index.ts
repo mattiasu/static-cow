a044ea9a6549f0b36c6a57a669c8cfafaf6348cc
Mattias Uhlegård <mattias@uhlegard.se>
2026-05-23 14:48:53 +0200

    404 for /api requests

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { handleApi } from "../functions/api";
+//import { handleApi } from "../functions/api";
 
 export default {
   async fetch(request: Request, env: Env): Promise<Response> {
     const url = new URL(request.url);
 
     if (url.pathname.startsWith("/api/")) {
-      return handleApi(request, env);
+      //return handleApi(request, env);
+	return new Repsonse("API coming soon", { status: 404});
     }
 
-    // Fallback to static assets
     return env.ASSETS.fetch(request);
   },
 };
